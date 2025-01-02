a69c86dfec5a2a7f95f4eb78b13af4445d90e390
João Pedro <jpguadalupe.ti@gmail.com>
2025-01-02 00:01:38 -0300

    text correction pt 2

--- keyLogic.py
+++ keyLogic.py
@@ -8,7 +8,7 @@ def getKey(player: Player, key: Key, screen: pygame.display.set_mode):
     player_rect = player.rect
     key_pressed = pygame.key.get_pressed()
     if player_rect.colliderect(key_rect):
-        displayText('Aperte E para pegar a chave')
+        displayText('Aperte E para pegar a chave', screen)
         if key_pressed[pygame.K_e]:
             player.wKey = True
             key.taken = True
@@ -16,6 +16,8 @@ def getKey(player: Player, key: Key, screen: pygame.display.set_mode):
 def useKey(player: Player, door_rect: pygame.Rect, screen: pygame.display.set_mode):
     player_rect = player.rect
     key_pressed = pygame.key.get_pressed()
-    if player_rect.colliderect(door_rect) and key_pressed[pygame.K_e] and player.wKey:
-        player.level += 1
+    if player_rect.colliderect(door_rect):
+        displayText('Aperte E para abrir a porta', screen)
+        if key_pressed[pygame.K_e] and player.wKey:
+            player.level += 1
         
